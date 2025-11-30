a528ba223b571dc421faf6cf63c7d6cbb22ec7c1
ItsNeil17 <neil@willofsteel.me>
2025-11-30 23:09:22 +0530

    feat: add redirect from what-we-know-so-far to established-information
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

--- next.config.mjs
+++ next.config.mjs
@@ -8,25 +8,20 @@ const config = {
   async redirects() {
     return [
       {
-        source: '/drakon-guide',
-        destination: 'https://docs.google.com/document/d/10BTtTlM0KlK18l-hLkU6-eRiWUTFljcvnaqF593cX8E/edit?tab=t.0',
-        permanent: false,
+      source: '/drakon-guide/:path*',
+      destination: 'https://docs.google.com/document/d/10BTtTlM0KlK18l-hLkU6-eRiWUTFljcvnaqF593cX8E/edit?tab=t.0',
+      permanent: false,
       },
       {
-        source: '/drakon-guide/:path*',
-        destination: 'https://docs.google.com/document/d/10BTtTlM0KlK18l-hLkU6-eRiWUTFljcvnaqF593cX8E/edit?tab=t.0',
-        permanent: false,
+      source: '/drakon-doc/:path*',
+      destination: 'https://docs.google.com/document/d/10BTtTlM0KlK18l-hLkU6-eRiWUTFljcvnaqF593cX8E/edit?tab=t.0',
+      permanent: false,
       },
       {
-        source: '/drakon-doc',
-        destination: 'https://docs.google.com/document/d/10BTtTlM0KlK18l-hLkU6-eRiWUTFljcvnaqF593cX8E/edit?tab=t.0',
-        permanent: false,
-      },
-      {
-        source: '/drakon-doc/:path*',
-        destination: 'https://docs.google.com/document/d/10BTtTlM0KlK18l-hLkU6-eRiWUTFljcvnaqF593cX8E/edit?tab=t.0',
-        permanent: false,
-      },
+      source: '/docs/what-we-know-so-far/:path*',
+      destination: '/docs/established-information/:path*',
+      permanent: false
+      }
     ]
   },
 };
