13e510fcbd9c24ea6c385b7d0b044aeb4e1d2e9d
Baran Mordoğan <baran@okunamayanad.com>
2025-12-09 13:31:50 +0300

    refactor: remove leftover code

--- src/app/(home)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ViewTransition } from "react";
-
-export default function HomeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ViewTransition>{children}</ViewTransition>;
-}
